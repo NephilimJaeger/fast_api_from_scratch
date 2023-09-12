fdcf57146af62ba9b675d845ff2d2550ffe9c64b
Nilane <nyllane16@gmail.com>
2023-09-11 21:08:16 -0300

    requests status

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -10,9 +11,17 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to my API"}
 
+# @app.get("/blog/all")
+# def get_all_blogs():
+#     return {"message":"All blogs provided"}
+
 @app.get("/blog/all")
-def get_all_blogs():
-    return {"message":"All blogs provided"}
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {"message":f"All {page_size} blogs on page {page}"}
+
+@app.get("/blog/{id}/{comment_id}")
+def get_commet(id:int,comment_id:int,valid:bool,username: Optional[str] = None):
+    return {"message":f"id {id}, comment_id {comment_id},valid {valid}, username {username}"}
 
 class BlogType(str, Enum):
     short = 'short'
@@ -23,7 +32,10 @@ class BlogType(str, Enum):
 def get_blog_type(type: BlogType):
     return {"message": f"Blog type {type.value}"}
 
-@app.get("/blog/{id}")
-def get_blog(id: int):
+@app.get("/blog/{id}", status_code= status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
     return {"message": f"Blog with id {id}"}
 
