2a8a87c86de9984e023cc085d885dc0f36d00e32
Nilane <nyllane16@gmail.com>
2023-10-10 09:35:57 -0300

    add a database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from router import blog_get, blog_post
+from database import models
+from database.db import engine
 
 app = FastAPI()
 app.include_router(blog_get.router)
@@ -9,6 +11,8 @@ app.include_router(blog_post.router)
 @app.get("/hello")
 def root():
     return {"message": "Welcome to my API"}
+
+models.Base.metadata.create_all(engine)
 
 
 
