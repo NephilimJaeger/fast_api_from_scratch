993908a2df2d3f90f9d94ba2c39d06b5ae0149a7
Nilane <nyllane16@gmail.com>
2023-11-14 18:02:19 -0300

    Custom responses

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from router import blog_get
 from router import blog_post
 from router import user
 from router import article
+from router import product
 from database import models
 from database.db import engine
 from exceptions import StoryException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import HTTPException
 
 
 app = FastAPI()
@@ -14,6 +16,7 @@ app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
 
 
 
@@ -25,6 +28,10 @@ def root():
 def story_exception_handler(request: Request, exec: StoryException):
     return JSONResponse(status_code=418,
                         content = {'detail':exec.name})
+
+# @app.exception_handler(HTTPException)
+# def custom_handler(request: Request, exec: HTTPException):
+#     return PlainTextResponse(str(exec),status_code=400)
 
 models.Base.metadata.create_all(engine)
 
